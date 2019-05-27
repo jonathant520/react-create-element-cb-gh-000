943bf47960ee3ed33c9419f0ab15de4e1f6175e1
jonathan thomas <jonathandthomas@icloud.com>
2019-05-27 01:29:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// Code goes here
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool Stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+ReactDom.render(
+  title,
+  document.getElementById('global')
+);
